e2146afabe4ecacf26552c7b71875017e000ced4
jarl <jarl@genecloud.com>
2015-09-23 12:30:46 -0700

    Trying the uploader.

--- uploader.sh
+++ uploader.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-## See AM2302.py for explanation of /tmp/measurements
-## Upload all the measurements we have collected, and remove the /tmp/measurements file
-curl -X POST --data-urlencode data@/tmp/measurements http://example.com/measurements > /dev/null 2>&1 rm /tmp/measurements
+## See AM2302.py for explanation of /tmp/measurements Upload all the
+## measurements we have collected, and remove the /tmp/measurements
+## file.  Stuff the return value in the file ''response''
+curl -s -X POST --data-urlencode data@/tmp/measurements http://example.com/measurements > response && rm /tmp/measurements
 
 ## Pictures???
-curl ----
+## curl ----
