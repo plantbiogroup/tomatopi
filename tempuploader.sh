c59d92ff5148a7665fa896a48906effd9f606ac2
jarl <jarl@genecloud.com>
2015-09-29 12:10:34 -0700

    New path.

--- tempuploader.sh
+++ tempuploader.sh
@@ -6,4 +6,4 @@
 ## See AM2302.py for explanation of /tmp/measurements Upload all the
 ## measurements we have collected, and remove the /tmp/measurements
 ## file.  Stuff the return value in the file ''response''
-curl -s -X POST --data-urlencode data@/tmp/measurements http://phytonaut/measurements > /tmp/response && rm /tmp/measurements
+/usr/bin/curl -s -X POST --data-urlencode data@/tmp/measurements http://phytonaut/measurements > /tmp/response && rm /tmp/measurements
